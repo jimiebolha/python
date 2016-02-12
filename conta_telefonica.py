b2abc7a7f1746abe861cbd95effcc2a40c7ec006
gnu <jimie.almeida@openmailbox.org>
2016-02-12 14:50:13 -0200

    Exercício lista telefônica

---
+++ conta_telefonica.py
@@ -0,0 +1,9 @@
+minutos = int(input("Digite a quantidade de minutos: "))
+if minutos < 200:
+	preco = 0.20
+else:
+	if minutos <= 400:
+		preco = 0.28
+	else:
+		preco = 0.15
+print ("O valor da sua conta telefonica e R$%2.2f" % (minutos * preco) )
