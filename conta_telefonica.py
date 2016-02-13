737659aa5f0174e0b56aeab72f2309f9621b0312
jimiebolha <jimie.almeida@openmailbox.org>
2016-02-13 10:12:03 -0200

    Alterando código com elif

--- conta_telefonica.py
+++ conta_telefonica.py
@@ -1,12 +1,11 @@
 minutos = int(input("Digite a quantidade de minutos: "))
 if minutos < 200:
 	preco = 0.20
+elif minutos <= 400:
+	preco = 0.28
+elif minutos <= 800:
+	preco = 0.15
 else:
-	if minutos <= 400:
-		preco = 0.28
-	else:
-		if minutos <= 800:
-			preco = 0.15
-		else:
-			preco = 0.08
+	preco = 0.08
+
 print ("O valor da sua conta telefonica e R$%2.2f" % (minutos * preco) )
